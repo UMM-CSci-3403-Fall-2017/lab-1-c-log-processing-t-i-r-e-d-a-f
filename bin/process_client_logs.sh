f4b905cbf5e17d9d46763815cd9d76369b911601
twarling <warli010@morris.umn.edu>
2017-09-05 11:18:54 -0500

    Created process_clients_logs.sh and began work on regex filter.

---
+++ bin/process_client_logs.sh
@@ -0,0 +1,9 @@
+#!/usr/bin/bash
+
+dir=$1
+
+cd $dir
+
+################IN PROGRESS (awk regex filter)########################
+
+#awk '(w+)\s(\d+)\s(\d{2}).* (\w+) from (\S+)' / > failed_login_data.txt
