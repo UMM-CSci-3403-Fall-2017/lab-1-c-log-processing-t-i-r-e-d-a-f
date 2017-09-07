67e5f46d9a1ddb9d3e3f049ecfd6f8881b184947
twarling <warli010@morris.umn.edu>
2017-09-07 11:56:18 -0500

    Fixed regex filter by adding gensub.

--- bin/process_client_logs.sh
+++ bin/process_client_logs.sh
@@ -8,6 +8,11 @@ cat * > mergeFile.txt
 
 mf=mergeFile.txt
 
-################IN PROGRESS (awk regex filter)########################
+################IN PROGRESS (awk regex filter)################hi########
+
+awk '/Failed password/' $mf > pwFail.txt
+
+awk '{ print gensub(/(\w+)\s+([[:digit:]]+)\s([[:digit:]]{2}).* Failed password .* (\w+) from (\S+).*/,  "\\1 \\2 \\3 \\4 \\5", "g");}' pwFail.txt > failed_login_data.txt
+
 
 #awk '/(\w+)\s(\d+)\s(\d{2}).* Failed password .* (\w+) from (\S+)/{print}' $mf > failed_login_data.txt
