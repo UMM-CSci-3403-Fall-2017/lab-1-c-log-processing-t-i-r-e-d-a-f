c1d190cd7f6098f4fc04ba5836373eb10e90c6a3
smil1201 <mill5978@morris.umn.edu>
2017-09-07 20:36:16 -0500

    Cleaned up unused code. Moved output file to correct location, tests now pass.

--- bin/process_client_logs.sh
+++ bin/process_client_logs.sh
@@ -1,4 +1,4 @@
-#!/usr/bin/bash
+#!/bin/bash
 
 dir=$1
 
@@ -8,11 +8,10 @@ cat * > mergeFile.txt
 
 mf=mergeFile.txt
 
-################IN PROGRESS (awk regex filter)################hi########
-
 awk '/Failed password/' $mf > pwFail.txt
 
 awk '{ print gensub(/(\w+)\s+([[:digit:]]+)\s([[:digit:]]{2}).* Failed password .* (\w+) from (\S+).*/,  "\\1 \\2 \\3 \\4 \\5", "g");}' pwFail.txt > failed_login_data.txt
 
+mv failed_login_data.txt $dir
 
-#awk '/(\w+)\s(\d+)\s(\d{2}).* Failed password .* (\w+) from (\S+)/{print}' $mf > failed_login_data.txt
+cd $dir
