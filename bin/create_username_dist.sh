bd879a08c7a4a835ff2b407475e70d803f026761
smil1201 <mill5978@morris.umn.edu>
2017-09-07 21:24:44 -0500

    Wrote the body of the function, still needs data generation for the contents file.

--- bin/create_username_dist.sh
+++ bin/create_username_dist.sh
@@ -1,1 +1,18 @@
 #!/usr/bin/bash
+
+#####in-progress, not functional yet########
+
+#store directory
+dir=$1
+
+#initialize body file that will contain the data.addRow lines
+contents=contents.txt
+
+#create data section from the summarized failed_login_data.txt files
+##TODO
+
+#wrap contents in the appropriate header and footer and place results in desired output file
+./bin/wrap_contents.sh $contents html_components/username_dist username_dist.html
+
+#move output file to the correct directory
+mv username_dist.html $dir
