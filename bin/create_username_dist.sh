3b0f9bf15e06e3b35b425587bc545009f8f428db
smil1201 <mill5978@morris.umn.edu>
2017-09-10 22:46:40 -0500

    Wrote data generation section and more comments, also removed unused code. All tests now pass.

--- bin/create_username_dist.sh
+++ bin/create_username_dist.sh
@@ -1,18 +1,25 @@
 #!/usr/bin/bash
 
-#####in-progress, not functional yet########
-
 #store directory
 dir=$1
 
-#initialize body file that will contain the data.addRow lines
-contents=contents.txt
+#concatenate all failed_login_data.txt files
+for i in $dir; do
+	cat $i/*/f* > merge_file.txt
+done
 
-#create data section from the summarized failed_login_data.txt files
-##TODO
+#turn our merged failed login files into a list of usernames and frequency count
+awk '{print $4}' merge_file.txt | sort | uniq -c > user_names.txt
+
+awk '{print "data.addRow([\x27"$2"\x27, "$1"]);"}' user_names.txt > user_counts.txt
 
 #wrap contents in the appropriate header and footer and place results in desired output file
-./bin/wrap_contents.sh $contents html_components/username_dist username_dist.html
+./bin/wrap_contents.sh user_counts.txt ./html_components/username_dist username_dist.html
+
+#remove intermediary files
+rm user_counts.txt
+rm user_names.txt
+rm merge_file.txt
 
 #move output file to the correct directory
 mv username_dist.html $dir
