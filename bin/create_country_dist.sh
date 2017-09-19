030f93e8e95fe8b4af35f9f0965cf54a6c01b296
twarling <warli010@morris.umn.edu>
2017-09-19 10:52:33 -0500

    Fixed bug, country_dist.html is now put in the right place. All tests pass.

--- bin/create_country_dist.sh
+++ bin/create_country_dist.sh
@@ -9,7 +9,6 @@ for i in $dir; do
 done
 
 #turn our merged failed login files into a list of usernames and frequency count
-#awk '{print $5}' merge_file.txt | sort | uniq -c > user_namesIP.txt
 awk '{print $5}' merge_file.txt | sort > user_namesIP.txt
 
 sort ./etc/country_IP_map.txt > IP_map.txt
@@ -17,15 +16,13 @@ sort ./etc/country_IP_map.txt > IP_map.txt
 #add country codes
 join -1 1 -2 1 user_namesIP.txt IP_map.txt > joined_IP.txt
 
-joined_IP.txt | sort | uniq -c > counted_IP.txt 
-
-#######################################IN PROGRESS-generates too many rows#################################################################
-
-#generate data rows
-awk '{print "data.addRow([\x27"$3"\x27, "$1"]);"}' counted_IP.txt > code_counts.txt
+awk '{print $2}' joined_IP.txt | sort | uniq -c > counted_IP.txt 
 
 #wrap contents in header and footer and place results in desired output file
 ./bin/wrap_contents.sh code_counts.txt ./html_components/country_dist country_dist.html
+
+#move output file to the correct directory
+mv country_dist.html $dir
 
 #remove intermediary files
 rm user_namesIP.txt
@@ -33,3 +30,4 @@ rm joined_IP.txt
 rm merge_file.txt
 rm IP_map.txt
 rm code_counts.txt
+rm counted_IP.txt
