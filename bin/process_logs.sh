4d49f8ee06d0fb722c8836564d306216c5f73141
smil1201 <mill5978@morris.umn.edu>
2017-09-19 19:07:22 -0500

    Added calls to all helper functions, tests are not passing yet.

--- bin/process_logs.sh
+++ bin/process_logs.sh
@@ -2,6 +2,7 @@
 
 #gather arguments
 files=$@
+dir=$(pwd)
 
 #make scratch directory
 scratch=`mktemp -d`
@@ -13,6 +14,20 @@ for var in $files; do
 	name=${filename%_*}
 
 	#make directory and place log files in them
-	mkdir scratch/$name
-	tar -zxf $var -C scratch/$name
+	mkdir $scratch/$name
+	tar -zxf $var -C $scratch/$name
+
+	#process client logs
+	./bin/process_client_logs.sh $scratch/$name
 done
+
+#create username hours and country distributions
+./bin/create_username_dist.sh $scratch
+./bin/create_hours_dist.sh $scratch
+./bin/create_country_dist.sh $scratch
+
+#combine result files
+./bin/assemble_report.sh $scratch
+
+#move summary file
+mv $scratch/failed_login_summary.html $dir
